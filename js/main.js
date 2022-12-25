e20d7807826e40be1878f86d15781b832975e014
Suxrob <suxrobjonb6@gamil.com>
2022-12-25 19:34:40 +0500

    frist commit

---
+++ js/main.js
@@ -0,0 +1,51 @@
+let elLoginBtn = document.querySelector(".login-btn");
+let elCloseBtn = document.querySelector(".site__exit");
+let elModal = document.querySelector(".login__modal");
+let elEye = document.querySelector(".eye__See");
+let elEyeNone = document.querySelector(".eye__See-none");
+let Body = document.querySelector("body");
+let password = document.querySelector(".password__input");
+let emailBtn = document.querySelector("email__input");
+let loginBtn = document.querySelector(".login__button-modal")
+
+eyeBtn.addEventListener('click', () => {
+    if(eyeBtn.getAttribute('src')=='./img/eye.svg'){
+        eyeBtn.setAttribute('src','./img/eye-2.svg')
+password.setAttribute('type','text')        
+}else{
+    eyeBtn.setAttribute('src','./img/eye.svg')
+    password.setAttribute('type','password')        
+    }
+})
+
+elLoginBtn.addEventListener("click", openModal);
+function openModal() {
+    elModal.style.display = 'block';
+}
+
+elCloseBtn.addEventListener("click", colseModal);
+function colseModal() {
+    elModal.style.display = 'none'
+}
+
+loginBtn.addEventListener("click", () => {
+    if(password > 8){
+        password.style.border = '2px solid red'
+    }
+})
+
+function validated(){
+    if (password.value.length < 9){
+        password.style.border = "2px solid #FF3B30";
+        password.focus()
+        return false
+    } 
+}
+
+// function validated(){
+//     if (emailBtn.value.length < 9){
+//         emailBtn.style.border = "2px solid #FF3B30";
+//         emailBtn.focus()
+//         return false
+//     } 
+// }
